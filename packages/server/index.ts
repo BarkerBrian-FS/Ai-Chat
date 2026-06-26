1dfcc2702f21e4bb0a87849c30b2172c87ff0a98
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-06-26 12:21:04 -0500

    added open ai and checked route with postman

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import type { Request, Response } from "express";
 import dotenv from "dotenv";
+import OpenAI from "openai";
 
 dotenv.config();
 
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
 const app = express();
+
+app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
@@ -14,6 +21,26 @@ app.get("/", (req: Request, res: Response) => {
 
 app.get("/api/hello", (req: Request, res: Response) => {
   res.json({ message: "Hello World" });
+});
+
+//conversation id => lastResponseId
+//
+const conversations = new Map<string, string>();
+
+app.post("/api/chat", async (req: Request, res: Response) => {
+  console.log(req.body);
+  const { prompt, conversationId } = req.body;
+
+  const response = await client.responses.create({
+    model: "gpt-5-mini",
+    input: prompt,
+    max_output_tokens: 100,
+    previous_response_id: conversations.get(conversationId),
+  });
+
+  conversations.set(conversationId, response.id);
+
+  res.json({ message: response.output_text });
 });
 
 app.listen(port, () => {
