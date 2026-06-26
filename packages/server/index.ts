120bc863fda401d63bf64bd38ac81ac4ac5aa30c
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-06-26 13:18:03 -0500

    added chat controller

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -2,7 +2,6 @@ import express from "express";
 import type { Request, Response } from "express";
 import dotenv from "dotenv";
 import z from "zod";
-import { chatService } from "./services/chat.service";
 
 dotenv.config();
 
@@ -20,33 +19,8 @@ app.get("/api/hello", (req: Request, res: Response) => {
   res.json({ message: "Hello World" });
 });
 
-const chatSchema = z.object({
-  prompt: z
-    .string()
-    .trim()
-    .min(1, "Prompt is required")
-    .max(1000, "Prompt is too long max 1000 char."),
-  conversationId: z.string().uuid(),
-});
-
 app.post("/api/chat", async (req: Request, res: Response) => {
-  const parseResult = chatSchema.safeParse(req.body);
-
-  if (!parseResult.success) {
-    res.status(400).json(parseResult.error.format());
-    return;
-  }
-
   console.log(req.body);
-
-  try {
-    const { prompt, conversationId } = req.body;
-    const response = await chatService.sendMessage(prompt, conversationId);
-
-    res.json({ message: response.message });
-  } catch (error) {
-    res.status(500).json({ error: "Failed to generate a response." });
-  }
 });
 
 app.listen(port, () => {
